b976b5bb98a1bbd58355c5323d5620aefe31f994
sele906 <seunga906@gmail.com>
2026-03-05 15:35:33 +0900

    cors vercel 허용

--- src/main/java/sele906/dev/beluo_backend/config/web/CorsConfig.java
+++ src/main/java/sele906/dev/beluo_backend/config/web/CorsConfig.java
@@ -1,6 +1,7 @@
 package sele906.dev.beluo_backend.config.web;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -11,11 +12,14 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${ALLOWED_ORIGINS}")
+    private String allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(allowedOrigins.split(",")));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
