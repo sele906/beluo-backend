8bf76b2a97e92693fdc7ba7090cfe459b378f43e
sele906 <seunga906@gmail.com>
2026-06-19 18:36:19 +0900

    FIREBASE 경로설정

--- src/main/java/sele906/dev/beluo_backend/config/firebase/FirebaseInitializer.java
+++ src/main/java/sele906/dev/beluo_backend/config/firebase/FirebaseInitializer.java
@@ -4,21 +4,37 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class FirebaseInitializer {
+
+    @Value("${FIREBASE_SERVICE_ACCOUNT:}")
+    private String firebaseServiceAccount;
 
     @PostConstruct
     public void init() {
         try {
 
-            InputStream serviceAccount =
-                    new ClassPathResource("firebase/firebase-service-account.json").getInputStream();
+            InputStream serviceAccount;
+
+            // 배포 환경
+            if (firebaseServiceAccount != null && !firebaseServiceAccount.isBlank()) {
+                serviceAccount = new ByteArrayInputStream(
+                        firebaseServiceAccount.getBytes(StandardCharsets.UTF_8));
+
+            // 로컬 환경
+            } else {
+                serviceAccount = new ClassPathResource(
+                        "firebase/firebase-service-account.json").getInputStream();
+            }
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
